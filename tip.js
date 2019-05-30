ab3bd1f31c77f075d3cf4b078a8126c48f8fbd4c
Brant Keener <brant_keener@yahoo.com>
2019-05-29 21:15:08 -0600

    Add gitignore and js comments

--- tip.js
+++ tip.js
@@ -6,7 +6,6 @@ const tipObject = {
   tip: 0,
   tipCalc: function() {
     let totalWTip = this.subtotal + (this.subtotal * (this.tip / 100));
-    // console.log(this.subtotal, this.tip)
     return totalWTip.toFixed(2);
   }
 };
@@ -15,9 +14,14 @@ const tipObject = {
 
 document.addEventListener('click', (event) => {
   event.preventDefault();
+
+  // Destructure the event.target
   const { value, id, tagName } = event.target;
   const subtotal = document.getElementById('subtotal').value;
   let tipEntry = document.getElementById('customTip');
+
+  // Check tagName to ensure that the "Tip Amount" section is being edited
+  // This will be used to turn on and off the custom tip input
   if(tagName === 'SELECT' || tagName === 'OPTION') {
     if(value === 'customSelected') {
       tipEntry.disabled = false;
@@ -25,6 +29,8 @@ document.addEventListener('click', (event) => {
       tipEntry.disabled = true;
     };
   };
+
+  // If the user hits the submit button, choose whether to use the generic number, or the custom number
   if(id === 'calculateTip') {
     tipObject.subtotal = parseFloat(subtotal);
     if(tipEntry.disabled === true) {
@@ -34,11 +40,9 @@ document.addEventListener('click', (event) => {
       const customTip = document.getElementById('customTip').value;
       tipObject.tip = parseInt(customTip);
     };
+
+    // Update the HTML appropriately
     let totalValue = `Total: ${tipObject.tipCalc()}`;
     document.getElementById('billTotal').textContent = totalValue
   };
 });
-
-
-// console.log(tipObject.tipCalc());
-// Display
